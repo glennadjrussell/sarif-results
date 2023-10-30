b25a7887ab9f7fc86e19a7a8eb58223265c8d8fd
Glenn Russell <glennadjrussell@gmail.com>
2023-10-30 11:38:14 +0000

    Ensuring that we set an output variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh -l
 
-cat $1 | jq --raw-output '\n"| SNYK ID | Short description | Full Description |",\n"|---------|------------------|------------------|",\n(.runs[].tool.driver.rules[] | ["|", .id, "|", .shortDescription.text, "|", .fullDescription.text, "|"] | join(" "))\n' >> $GITHUB_OUTPUT
+formatted=$(cat $1 | jq --raw-output '\n"| SNYK ID | Short description | Full Description |",\n"|---------|------------------|------------------|",\n(.runs[].tool.driver.rules[] | ["|", .id, "|", .shortDescription.text, "|", .fullDescription.text, "|"] | join(" "))\n')
+
+echo "sarif_json=$formatted" >> $GITHUB_OUTPUT
 
