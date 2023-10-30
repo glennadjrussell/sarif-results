efbf72eacd88b7f802aa2a23ee9daa5a3ce73e65
Glenn Russell <glennadjrussell@gmail.com>
2023-10-30 15:36:58 +0000

    Adding multiline logic

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,7 @@
 
 formatted=$(cat snyk.sarif | jq --raw-output '"| SNYK ID | Short description | Full Description |","|---------|------------------|------------------|",(.runs[].tool.driver.rules[] | ["|", .id, "|", .shortDescription.text, "|", .fullDescription.text, "|"] | join(" "))')
 
-echo "sarif_json=$formatted" >> $GITHUB_OUTPUT
+echo "sarif_json<<EOF" >> $GITHUB_OUTPUT
+echo "$formatted" >> $GITHUB_OUTPUT
+echo "EOF" >> $GITHUB_OUTPUT
 
