6eac0ad847e47a590ac8c83db7623840851b777a
Glenn Russell <glennadjrussell@gmail.com>
2023-11-09 23:25:29 +0000

    The script now works with two forms of inputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,19 @@
 #!/bin/sh -l
 
-formatted=$(cat snyk.sarif | jq --raw-output '"| SNYK ID | Short description | Full Description |","|---------|------------------|------------------|",(.runs[].tool.driver.rules[] | ["|", .id, "|", .shortDescription.text, "|", .fullDescription.text, "|"] | join(" "))')
+case "$1" in
+  sca)
+    formatted=$(cat "$2" | jq --raw-output '"| SNYK ID | Short description | Full Description |","|---------|------------------|------------------|",(.runs[].tool.driver.rules[] | ["|", .id, "|", .shortDescription.text, "|", .fullDescription.text, "|"] | join(" "))')
+    ;;
+  code)
+    formatted=$(cat "$2" | jq --raw-output '"| SNYK Rule ID | Source file | Line |","|---------|------------------|------------------|",(.runs[].results[] | ["|", .ruleId, "|", .locations[].physicalLocation.artifactLocation.uri, "|", .locations[].physicalLocation.region.startLine, "|"] | join(" "))')
+    ;;
+  *)
+    exit 1
+esac
 
 echo "sarif_json<<EOF" >> $GITHUB_OUTPUT
 echo "$formatted" >> $GITHUB_OUTPUT
 echo "EOF" >> $GITHUB_OUTPUT
+
+exit 0
 
