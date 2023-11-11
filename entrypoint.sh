e7b5df25d1d8d4fee15cfef27f5e5f24f7a94b07
Glenn Russell <glennadjrussell@gmail.com>
2023-11-11 12:10:13 +0000

    Fixing formatting

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,17 +8,18 @@ case "$1" in
       echo "status=$status'" >> $GITHUB_OUTPUT
       formatted="$status"
     else
+      echo "status='\u274C SCA issues reported'" >> $GITHUB_OUTPUT
       formatted=$(cat "$2" | jq --raw-output '"| SNYK ID | Short description | Full Description |","|---------|------------------|------------------|",( .runs[].tool.driver.rules[] | ["|", .id, "|", .shortDescription.text, "|", .fullDescription.text, "|"] | join(" "))')
     fi
     ;;
   code)
     results=$(cat "$2" | jq '.runs[].results[]')
     if [ -z "$results" ]; then
-      status="\u2705 No SCA issues reported"
+      status="\u2705 No code issues reported"
       formatted="$status"
       echo "status=$status" >> $GITHUB_OUTPUT
     else
-      echo "status='\u274C SCA issues reported'" >> $GITHUB_OUTPUT
+      echo "status='\u274C code issues reported'" >> $GITHUB_OUTPUT
       formatted=$(cat "$2" | jq --raw-output '"| SNYK ID | Short description | Full Description |","|---------|------------------|------------------|",(.runs[].results[] | ["|", .ruleId, "|", .locations[].physicalLocation.artifactLocation.uri, "|", .locations[].physicalLocation.region.startLine, "|"] | join(" "))')
     fi
     ;;
@@ -27,12 +28,14 @@ case "$1" in
 esac
 
 echo "sarif_json<<EOF" >> $GITHUB_OUTPUT
-echo "<detail>" >> $GITHUB_OUTPUT
+echo "<details>" >> $GITHUB_OUTPUT
 echo "<summary>" >> $GITHUB_OUTPUT
 echo "Results" >> $GITHUB_OUTPUT
 echo "</summary>" >> $GITHUB_OUTPUT
+echo "" >> $GITHUB_OUTPUT
 echo "$formatted" >> $GITHUB_OUTPUT
-echo "</detail>" >> $GITHUB_OUTPUT
+echo "" >> $GITHUB_OUTPUT
+echo "</details>" >> $GITHUB_OUTPUT
 echo "EOF" >> $GITHUB_OUTPUT
 
 exit 0
